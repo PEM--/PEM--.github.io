198d002c0685edf6376c92908c540e2e3071a564
PEM-- <pemarchandet@gmail.com>
2018-09-17 23:11:50 +0200

    Layouts - Early site nav

--- src/src/layouts/index.js
+++ src/src/layouts/index.js
@@ -11,6 +11,29 @@ const LayoutDiv = styled.div`
   width: 960px;
 `
 
+const Nav = styled.nav`
+  background: aliceblue;
+`
+
+const Header = styled.header`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+  max-width: 960px;
+  >* {
+    display: block;
+    flex: 0 0 auto;
+  }
+`
+
+const LinkList = styled.ul`
+  list-style-type: none;
+  margin: auto;
+  white-space: nowrap;
+  >li { margin: 0; }
+`
+
 const Layout = ({ children }) => (<Fragment>
   <StaticQuery
     query={graphql`
@@ -22,13 +45,21 @@ const Layout = ({ children }) => (<Fragment>
         }
       }
     `}
-    render={data => <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />}
+    render={({ site: { siteMetadata: { title } } }) => (<Nav>
+      <Helmet
+        title={title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Header>
+        <Link to='/'><h1>{title}</h1></Link>
+        <LinkList>
+          <li><Link to='/about'>About</Link></li>
+        </LinkList>
+      </Header>
+  </Nav>)}
   />
   <Main>
     <LayoutDiv>{children}</LayoutDiv>
--- src/src/pages/index.js
+++ src/src/pages/index.js
@@ -2,10 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import Layout from '../layouts'
-import Main from '../components/main'
 
 export default () => (<Layout>
-  <h1>PEM's blog</h1>
+  <h1>List of posts</h1>
   <p>Some random thoughts and notes about dev, Math, ML & AI</p>
   <Link to='/about'>About</Link>
 </Layout>)
