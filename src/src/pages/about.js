d44025dc999b7cd9e7f6ab3a5caaaac08ced42fc
PEM-- <pemarchandet@gmail.com>
2018-09-17 22:43:38 +0200

    Checking GraphQL

--- src/src/pages/about.js
+++ src/src/pages/about.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { graphql } from 'gatsby'
 
 import Layout from '../layouts'
-import Main from '../components/main'
 
-export default () => (<Layout>
-  <h1>About me</h1>
+export default ({ data: { site: { siteMetadata: { title } }} }) => (<Layout>
+  <h1>About {title}</h1>
   <Link to='/'>Home</Link>
 </Layout>)
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
