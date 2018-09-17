7a5c74f53e7302f23180cfb5ea97ead4d84f506e
PEM-- <pemarchandet@gmail.com>
2018-09-17 22:52:46 +0200

    Layouts - Add helmet for site title

--- src/src/layouts/index.js
+++ src/src/layouts/index.js
@@ -1,67 +1,39 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
+import { StaticQuery, Link, graphql } from  'gatsby'
 import { element } from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Main from '../components/main'
-
-// import Header from '../components/header'
-
-// const Layout = ({ children, data }) => (
-//   <div>
-//     <Helmet
-//       title={data.site.siteMetadata.title}
-//       meta={[
-//         { name: 'description', content: 'Sample' },
-//         { name: 'keywords', content: 'sample, something' },
-//       ]}
-//     />
-//     <Header siteTitle={data.site.siteMetadata.title} />
-//     <div
-//       style={{
-//         margin: '0 auto',
-//         maxWidth: 960,
-//         padding: '0px 1.0875rem 1.45rem',
-//         paddingTop: 0,
-//       }}
-//     >
-//       {children()}
-//     </div>
-//   </div>
-// )
-
-// Layout.propTypes = {
-//   children: PropTypes.func,
-// }
-
-// export default Layout
-
-// export const query = graphql`
-//   query SiteTitleQuery {
-//     site {
-//       siteMetadata {
-//         title
-//       }
-//     }
-//   }
-// `
-
 
 const LayoutDiv = styled.div`
   margin: 1em auto;
   width: 960px;
 `
 
-const Layout = ({ children }) => (<Main>
-  {/* <Helmet
-    title={data.site.siteMetadata.title}
-    meta={[
-      { name: 'description', content: 'Sample' },
-      { name: 'keywords', content: 'sample, something' },
-    ]}
-  /> */}
-  <LayoutDiv>{children}</LayoutDiv>
-</Main>)
+const Layout = ({ children }) => (<Fragment>
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />}
+  />
+  <Main>
+    <LayoutDiv>{children}</LayoutDiv>
+  </Main>
+</Fragment>)
 Layout.propTypes = {
   childre: element.isRequired
 }
