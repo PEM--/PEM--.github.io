f75d4cec0ca3dc3a6c9c4a728e2871da318851d7
PEM-- <pemarchandet@gmail.com>
2018-09-18 10:26:49 +0200

    WebApp manifest

--- src/gatsby-config.js
+++ src/gatsby-config.js
@@ -1,6 +1,10 @@
+const title = "PEM's blog"
+
+const faviconPath =  file => `src/favicons/${file}`
+
 module.exports = {
   siteMetadata: {
-    title: "PEM's blog",
+    title,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,6 +20,40 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: title,
+        short_name: 'PEM',
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `minimal-ui`,
+        icon: faviconPath('favicon-32x32.png'),
+        icons: [
+          {
+            src: faviconPath('android-chrome-192x192.png'),
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: faviconPath('android-chrome-256x256.png'),
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: faviconPath('android-chrome-32x32.png'),
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: faviconPath('android-chrome-16x16.png'),
+            sizes: `16x16`,
+            type: `image/png`,
+          }
+        ]
       },
     }
   ],
