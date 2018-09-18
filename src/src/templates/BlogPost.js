7112f2c2e076a1ce081ad368de6d66f175eb0487
PEM-- <pemarchandet@gmail.com>
2018-09-18 21:26:06 +0200

    Templates/BlogPost - Add specific title & description in head

--- src/src/templates/BlogPost.js
+++ src/src/templates/BlogPost.js
@@ -1,10 +1,11 @@
-import React from 'react'
 import Disqus from 'disqus-react'
+import Helmet from 'react-helmet'
+import React from 'react'
 import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 
-export default function BlogPost ({ data: { markdownRemark: post, site }, location, ...others }) {
+export default function BlogPost ({ data: { markdownRemark: post, site }, location }) {
   const discussProps = {
     config: {
       identifier: post.id,
@@ -13,7 +14,15 @@ export default function BlogPost ({ data: { markdownRemark: post, site }, locati
     },
     shortname: site.siteMetadata.discussShortName
   }
+  console.log('post', post)
+  console.log('site', site)
   return (<Layout>
+    <Helmet
+      title={`${site.siteMetadata.title}: ${post.frontmatter.title}`}
+      meta={[
+        { name: 'description', content: post.frontmatter.description }
+      ]}
+    />
     <h1>{post.frontmatter.title}</h1>
     <div dangerouslySetInnerHTML={{ __html: post.html }} />
     <Disqus.DiscussionEmbed {...discussProps} />
@@ -25,13 +34,15 @@ query ($slug: String!) {
   site {
     siteMetadata {
       discussShortName
+      title
     }
   }
   markdownRemark(fields: {slug: {eq: $slug}}) {
-    html
-    id
     frontmatter {
+      description
       title
     }
+    html
+    id
   }
 }`
