411b1fd2dc5efb690f864f212b61294ae008d137
PEM-- <pemarchandet@gmail.com>
2018-09-19 22:05:39 +0200

    Circumvent gatsby-mdx issue with passing options to gatsby-remark-images

--- src/src/themes/GlobalStyle.js
+++ src/src/themes/GlobalStyle.js
@@ -4,5 +4,14 @@ const GlobalStyle = createGlobalStyle /* css */ `
   html, body, #___gatsby, #___gatsby>div {
     height: 100%;
   }
+  div > p > .gatsby-resp-image-link {
+    pointer-events: none;
+    .gatsby-resp-image-wrapper {
+      border: 1px solid ${({ theme }) => theme.lightGray};
+      border-radius: 5px;
+      max-width: 100% !important;
+      overflow: hidden;
+    }
+  }
 `
 export default GlobalStyle
