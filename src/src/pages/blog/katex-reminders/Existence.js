b4ec5cb2627292f1e04b16d85d18c91272bd4a53
PEM-- <pemarchandet@gmail.com>
2018-09-20 14:49:31 +0200

    Katex reminder - Existence & condition

---
+++ src/src/pages/blog/katex-reminders/Existence.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Katex from '../../../components/Katex'
+
+export default function () {
+  return <Katex
+    displayMode
+    text={String.raw`
+      \forall~x \in \reals, \exist ~a ~and~ b \in \N ~where~
+      x = \begin{cases}
+        a < x \\
+        x < b
+      \end{cases}
+    `}
+  />
+}
