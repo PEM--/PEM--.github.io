8fe547c53f20c5e70aa506f68d218f0d099aec12
PEM-- <pemarchandet@gmail.com>
2018-09-20 08:33:57 +0200

    Component - Katex - displayMaode

--- src/src/components/Katex.js
+++ src/src/components/Katex.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import katex from 'katex'
+import { boolean, string } from 'prop-types'
 
 export default class Katex extends Component {
+  static propTypes = {
+    displayMode: boolean,
+    text: string.isRequired
+  }
   constructor (props) {
     super(props)
-    this.content = katex.renderToString(props.text)
+    this.content = katex.renderToString(props.text, { displayMode: props.displayMode })
   }
   shouldComponentUpdate = () => false
   render () {
